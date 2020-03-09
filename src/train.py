6daaaee61bbe9722083c0c63019396a51f9007b1
Subash Gandyer <vejeysubash@gmail.com>
2020-03-09 16:13:02 -0400

    Added train

--- src/train.py
+++ src/train.py
@@ -6,6 +6,9 @@ import os
 TRAINING_DATA = os.environ.get("TRAINING_DATA")
 FOLD = int(os.environ.get("FOLD"))
 
+print(TRAINING_DATA)
+print(FOLD)
+
 FOLD_MAPPING = {
     0: [1, 2, 3, 4],
     1: [0, 2, 3, 4],
@@ -34,9 +37,10 @@ if __name__ == "__main__":
         valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
         label_encoders.append((c, lbl))
 
-    clf = ensemble.RandomForerstClassifier(n_jobs=-1, verbose=2)
+    clf = ensemble.RandomForestClassifier(n_jobs=-1, verbose=2)
     clf.fit(train_df, ytrain)
     preds = clf.predict_proba(valid_df)[:, 1]
-    print(preds)
+    # print(preds)
+    print(metrics.roc_auc_score(yvalid, preds))
 
 
