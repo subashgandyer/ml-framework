71c49141d0aceaee4dd6e930054b710ee98dfbde
Subash Gandyer <vejeysubash@gmail.com>
2020-03-09 17:32:11 -0400

    Dispatcher script

--- src/create_folds.py
+++ src/create_folds.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     kf = model_selection.StratifiedKFold(n_splits=5, shuffle=False, random_state=42)
 
     for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df.target.values)):
-        print(len(train_idx), len(val_idx))
+        # print(len(train_idx), len(val_idx))
         df.loc[val_idx, "kfold"] = fold
 
     df.to_csv("input/train_folds.csv", index=False)
--- src/train.py
+++ src/train.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from sklearn import ensemble
 from sklearn import preprocessing
+from sklearn import metrics
 import os
+
+from . import dispatcher
 
 TRAINING_DATA = os.environ.get("TRAINING_DATA")
 FOLD = int(os.environ.get("FOLD"))
+MODEL = os.environ.get("MODEL")
 
 print(TRAINING_DATA)
 print(FOLD)
@@ -16,6 +20,7 @@ FOLD_MAPPING = {
     3: [0, 1, 2, 4],
     4: [0, 1, 2, 3]
 }
+
 if __name__ == "__main__":
     df = pd.read_csv(TRAINING_DATA)
     train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))]
@@ -23,6 +28,8 @@ if __name__ == "__main__":
 
     ytrain = train_df.target.values
     yvalid = valid_df.target.values
+
+    print(ytrain.shape, yvalid.shape)
 
     train_df = train_df.drop(["id", "target", "kfold"], axis=1)
     valid_df = valid_df.drop(["id", "target", "kfold"], axis=1)
@@ -32,12 +39,14 @@ if __name__ == "__main__":
     label_encoders = []
     for c in train_df.columns:
         lbl = preprocessing.LabelEncoder()
+        train_df.loc[:, c] = train_df.loc[:, c].astype(str).fillna("NONE")
+        valid_df.loc[:, c] = valid_df.loc[:, c].astype(str).fillna("NONE")
         lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
         train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
         valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
         label_encoders.append((c, lbl))
 
-    clf = ensemble.RandomForestClassifier(n_jobs=-1, verbose=2)
+    clf = dispatcher.MODELS[MODEL]
     clf.fit(train_df, ytrain)
     preds = clf.predict_proba(valid_df)[:, 1]
     # print(preds)
